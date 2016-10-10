4d711eb36345fb6c2634af7add220813672daffb
Hristo Oskov <hristo@kryptnostic.com>
2016-10-10 14:46:33 -0700

    separate build and build:min into individual build steps

--- src/utils/LangUtils.js
+++ src/utils/LangUtils.js
@@ -4,7 +4,7 @@
 
 import * as _ from 'lodash';
 
-export function isNonEmptyString(value :any) :boolean {
+export function isNonEmptyString(value :any) :boolean { // eslint-disable-line
 
   return _.isString(value) && !_.isEmpty(value);
 }
