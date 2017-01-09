8a89a3c08366cf76a7996c22fd9a1c8830304be1
Hristo Oskov <hristo@kryptnostic.com>
2017-01-09 11:51:45 -0800

    PropertyType

---
+++ src/models/PropertyType.js
@@ -0,0 +1,150 @@
+/*
+ * @flow
+ */
+
+import Immutable from 'immutable';
+
+import FullyQualifiedName from './FullyQualifiedName';
+
+import {
+  isNonEmptyArray,
+  isNonEmptyString,
+  isValidUUID
+} from '../utils/LangUtils';
+
+/**
+ * @class PropertyType
+ * @memberof loom-data
+ */
+class PropertyType {
+
+  id :?UUID;
+  type :FullyQualifiedName;
+  title :string;
+  description :string;
+  datatype :string;
+  schemas :Set<FullyQualifiedName>;
+
+  constructor(
+      id :?UUID,
+      type :FullyQualifiedName,
+      title :string,
+      description :string,
+      datatype :string,
+      schemas :Set<FullyQualifiedName>) {
+
+    this.id = id;
+    this.type = type;
+    this.title = title;
+    this.description = description;
+    this.datatype = datatype;
+    this.schemas = schemas;
+  }
+}
+
+/**
+ * @class PropertyTypeBuilder
+ * @memberof loom-data
+ */
+export default class PropertyTypeBuilder {
+
+  id :?UUID;
+  type :FullyQualifiedName;
+  title :string;
+  description :string;
+  datatype :string;
+  schemas :Set<FullyQualifiedName>;
+
+  setId(propertyTypeId :UUID) :PropertyTypeBuilder {
+
+    if (!isValidUUID(propertyTypeId)) {
+      throw new Error('invalid parameter: propertyTypeId must be a valid UUID');
+    }
+
+    this.id = propertyTypeId;
+    return this;
+  }
+
+  setType(propertyTypeFqn :FullyQualifiedName) :PropertyTypeBuilder {
+
+    if (!FullyQualifiedName.isValidFqn(propertyTypeFqn.getFullyQualifiedName())) {
+      throw new Error('invalid parameter: propertyTypeFqn must be a valid FQN');
+    }
+
+    this.type = propertyTypeFqn;
+    return this;
+  }
+
+  setTitle(title :string) :PropertyTypeBuilder {
+
+    if (!isNonEmptyString(title)) {
+      throw new Error('invalid parameter: title must be a non-empty string');
+    }
+
+    this.title = title;
+    return this;
+  }
+
+  setDescription(description :string) :PropertyTypeBuilder {
+
+    if (!isNonEmptyString(description)) {
+      throw new Error('invalid parameter: description must be a non-empty string');
+    }
+
+    this.description = description;
+    return this;
+  }
+
+  setDataType(datatype :string) :PropertyTypeBuilder {
+
+    if (!isNonEmptyString(datatype)) {
+      throw new Error('invalid parameter: datatype must be a non-empty string');
+    }
+
+    this.datatype = datatype;
+    return this;
+  }
+
+  setSchemas(schemas :FullyQualifiedName[]) :PropertyTypeBuilder {
+
+    if (!isNonEmptyArray(schemas)) {
+      throw new Error('invalid parameter: schemas must be a non-empty array');
+    }
+
+    let errorMessage = '';
+    const allValid = schemas.reduce((isValid, shemaFqn, index) => {
+      if (!isValid) {
+        return false;
+      }
+      if (!FullyQualifiedName.isValidFqn(shemaFqn.getFullyQualifiedName())) {
+        errorMessage = `invalid parameter: schemas[${index}] must be a valid FQN`;
+        return false;
+      }
+      return isValid;
+    }, true);
+
+    if (!allValid) {
+      throw new Error(errorMessage);
+    }
+
+    this.schemas = Immutable.Set().withMutations((set :Set<FullyQualifiedName>) => {
+      schemas.forEach((schemaFqn :FullyQualifiedName) => {
+        set.add(schemaFqn);
+      });
+    });
+
+    return this;
+  }
+
+  build() :PropertyType {
+
+    return new PropertyType(
+      this.id,
+      this.type,
+      this.title,
+      this.description,
+      this.datatype,
+      this.schemas
+    );
+  }
+}
