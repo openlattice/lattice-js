5f44e9092d960d5ec49e01a4115c5b0b7b3fef39
Hristo Oskov <hristo@kryptnostic.com>
2017-01-12 10:03:01 -0800

    test public api exposes models

---
+++ test/models/index.test.js
@@ -0,0 +1,100 @@
+import * as DataModels from '../../src/models/index';
+
+describe('Loom.DataModels', () => {
+
+  it('should expose FullyQualifiedName', () => {
+    expect(DataModels.FullyQualifiedName).toBeDefined();
+    expect(DataModels.FullyQualifiedName).toEqual(jasmine.any(Function));
+  });
+
+  it('should expose Ace', () => {
+    expect(DataModels.Ace).toBeDefined();
+    expect(DataModels.Ace).toEqual(jasmine.any(Function));
+  });
+
+  it('should expose AceBuilder', () => {
+    expect(DataModels.AceBuilder).toBeDefined();
+    expect(DataModels.AceBuilder).toEqual(jasmine.any(Function));
+  });
+
+  it('should expose Acl', () => {
+    expect(DataModels.Acl).toBeDefined();
+    expect(DataModels.Acl).toEqual(jasmine.any(Function));
+  });
+
+  it('should expose AclBuilder', () => {
+    expect(DataModels.AclBuilder).toBeDefined();
+    expect(DataModels.AclBuilder).toEqual(jasmine.any(Function));
+  });
+
+  it('should expose AclData', () => {
+    expect(DataModels.AclData).toBeDefined();
+    expect(DataModels.AclData).toEqual(jasmine.any(Function));
+  });
+
+  it('should expose AclDataBuilder', () => {
+    expect(DataModels.AclDataBuilder).toBeDefined();
+    expect(DataModels.AclDataBuilder).toEqual(jasmine.any(Function));
+  });
+
+  it('should expose AclKeyFragment', () => {
+    expect(DataModels.AclKeyFragment).toBeDefined();
+    expect(DataModels.AclKeyFragment).toEqual(jasmine.any(Function));
+  });
+
+  it('should expose AclKeyFragmentBuilder', () => {
+    expect(DataModels.AclKeyFragmentBuilder).toBeDefined();
+    expect(DataModels.AclKeyFragmentBuilder).toEqual(jasmine.any(Function));
+  });
+
+  it('should expose EntitySet', () => {
+    expect(DataModels.EntitySet).toBeDefined();
+    expect(DataModels.EntitySet).toEqual(jasmine.any(Function));
+  });
+
+  it('should expose EntitySetBuilder', () => {
+    expect(DataModels.EntitySetBuilder).toBeDefined();
+    expect(DataModels.EntitySetBuilder).toEqual(jasmine.any(Function));
+  });
+
+  it('should expose EntityType', () => {
+    expect(DataModels.EntityType).toBeDefined();
+    expect(DataModels.EntityType).toEqual(jasmine.any(Function));
+  });
+
+  it('should expose EntityTypeBuilder', () => {
+    expect(DataModels.EntityTypeBuilder).toBeDefined();
+    expect(DataModels.EntityTypeBuilder).toEqual(jasmine.any(Function));
+  });
+
+  it('should expose Principal', () => {
+    expect(DataModels.Principal).toBeDefined();
+    expect(DataModels.Principal).toEqual(jasmine.any(Function));
+  });
+
+  it('should expose PrincipalBuilder', () => {
+    expect(DataModels.PrincipalBuilder).toBeDefined();
+    expect(DataModels.PrincipalBuilder).toEqual(jasmine.any(Function));
+  });
+
+  it('should expose PropertyType', () => {
+    expect(DataModels.PropertyType).toBeDefined();
+    expect(DataModels.PropertyType).toEqual(jasmine.any(Function));
+  });
+
+  it('should expose PropertyTypeBuilder', () => {
+    expect(DataModels.PropertyTypeBuilder).toBeDefined();
+    expect(DataModels.PropertyTypeBuilder).toEqual(jasmine.any(Function));
+  });
+
+  it('should expose Schema', () => {
+    expect(DataModels.Schema).toBeDefined();
+    expect(DataModels.Schema).toEqual(jasmine.any(Function));
+  });
+
+  it('should expose SchemaBuilder', () => {
+    expect(DataModels.SchemaBuilder).toBeDefined();
+    expect(DataModels.SchemaBuilder).toEqual(jasmine.any(Function));
+  });
+
+});
