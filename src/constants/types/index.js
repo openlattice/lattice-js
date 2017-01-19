d53cded94234c399cfcb07b03f443c68713f1fe7
Hristo Oskov <hristo@kryptnostic.com>
2017-01-19 12:47:47 -0800

    export flow types

--- src/constants/types/index.js
+++ src/constants/types/index.js
@@ -8,10 +8,38 @@ import PrincipalTypes from './PrincipalTypes';
 import RequestStatusTypes from './RequestStatusTypes';
 import SecurableTypes from './SecurableTypes';
 
+import type {
+  Action
+} from './ActionTypes';
+
+import type {
+  Permission
+} from './PermissionTypes';
+
+import type {
+  PrincipalType
+} from './PrincipalTypes';
+
+import type {
+  RequestStatus
+} from './RequestStatusTypes';
+
+import type {
+  SecurableType
+} from './SecurableTypes';
+
 export {
   ActionTypes,
   PermissionTypes,
   PrincipalTypes,
   RequestStatusTypes,
   SecurableTypes
+};
+
+export type {
+  Action,
+  Permission,
+  PrincipalType,
+  RequestStatus,
+  SecurableType
 };
