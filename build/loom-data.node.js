cd5927e69fbc92835e68c82179ff6c7a71424a17
Hristo Oskov <hristo@kryptnostic.com>
2017-01-20 13:20:29 -0800

    releasing v0.13.5

--- build/loom-data.node.js
+++ build/loom-data.node.js
@@ -1,6 +1,6 @@
 /*!
  * 
- * loom-data - v0.13.4
+ * loom-data - v0.13.5
  * JavaScript SDK for all Loom REST APIs
  * https://github.com/kryptnostic/loom-data-js
  * 
@@ -48608,7 +48608,7 @@ var _Configuration = __webpack_require__(32);
 
 function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) newObj[key] = obj[key]; } } newObj.default = obj; return newObj; } }
 
-var version = "v0.13.4";
+var version = "v0.13.5";
 
 /**
  * The `loom-data` library is a layer on top of Loom's REST APIs to simplify the process of reading data from and
