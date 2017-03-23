177c7d888f826c36b3442aebe13992cf60b28756
Hristo Oskov <hristo@kryptnostic.com>
2017-03-22 21:20:43 -0700

    reordering ApiPaths constants

--- test/constants/ApiPaths.test.js
+++ test/constants/ApiPaths.test.js
@@ -1,62 +1,111 @@
 import * as ApiPaths from '../../src/constants/ApiPaths';
 
-// shared paths
+// base endpoint controller paths
+const AUTHORIZATIONS_PATH :string = 'authorizations';
+const DATA_PATH :string = 'data';
+const DATA_SOURCES_PATH :string = 'datasource';
 const DATASTORE_PATH :string = 'datastore';
+const EDM_PATH :string = 'edm';
+const LINKING_PATH :string = 'linking';
+const ORGANIZATIONS_PATH :string = 'organizations';
+const PERMISSIONS_PATH :string = 'permissions';
+const PRINCIPALS_PATH :string = 'principals';
+const REQUESTS_PATH :string = 'requests';
+const SEARCH_PATH :string = 'search';
+
+// shared paths
 const ENTITY_SET_PATH :string = 'entity/set';
 const ENTITY_TYPE_PATH :string = 'entity/type';
 const IDS_PATH :string = 'ids';
 const NAMESPACE_PATH :string = 'namespace';
-const PRINCIPALS_PATH :string = 'principals';
 const PROPERTY_TYPE_PATH :string = 'property/type';
 const ROLES_PATH :string = 'roles';
 const SET_PATH :string = 'set';
 const TYPE_PATH :string = 'type';
 
-// AuthorizationApi specific paths
-const AUTHORIZATIONS_PATH :string = 'authorizations';
-
 // DataApi specific paths
-const DATA_PATH :string = 'data';
 const ENTITY_DATA_PATH :string = 'entitydata';
 const TICKET_PATH :string = 'ticket';
 
-// DataSourcesApi specific paths
-const DATA_SOURCES_PATH :string = 'datasource';
-
 // EntityDataModelApi specific paths
-const EDM_PATH :string = 'edm';
 const SCHEMA_PATH :string = 'schema';
-
-// LinkingApi specific paths
-const LINKING_PATH :string = 'linking';
 
 // OrganizationsApi specific paths
 const DESCRIPTION_PATH :string = 'description';
 const EMAIL_DOMAINS_PATH :string = 'email-domains';
 const MEMBERS_PATH :string = 'members';
-const ORGANIZATIONS_PATH :string = 'organizations';
 const TITLE_PATH :string = 'title';
-
-// PermissionsApi specific paths
-const PERMISSIONS_PATH :string = 'permissions';
 
 // PrincipalsApi specific paths
 const USERS_PATH :string = 'users';
 
-// RequestsApi specific paths
-const REQUESTS_PATH :string = 'requests';
-
 // SearchApi specific paths
-const SEARCH_PATH :string = 'search';
+const ADVANCED_PATH :string = 'advanced';
+const SEARCH_ENTITY_TYPES_PATH :string = 'entity_types';
+const SEARCH_PROPERTY_TYPES_PATH :string = 'property_types';
 
 describe('ApiPaths', () => {
 
-  describe('shared paths', () => {
+  describe('base endpoint controller paths', () => {
+
+    it(`should expose "${AUTHORIZATIONS_PATH}"`, () => {
+      expect(ApiPaths.AUTHORIZATIONS_PATH).toBeDefined();
+      expect(ApiPaths.AUTHORIZATIONS_PATH).toEqual(AUTHORIZATIONS_PATH);
+    });
+
+    it(`should expose "${DATA_PATH}"`, () => {
+      expect(ApiPaths.DATA_PATH).toBeDefined();
+      expect(ApiPaths.DATA_PATH).toEqual(DATA_PATH);
+    });
+
+    it(`should expose "${DATA_SOURCES_PATH}"`, () => {
+      expect(ApiPaths.DATA_SOURCES_PATH).toBeDefined();
+      expect(ApiPaths.DATA_SOURCES_PATH).toEqual(DATA_SOURCES_PATH);
+    });
 
     it(`should expose "${DATASTORE_PATH}"`, () => {
       expect(ApiPaths.DATASTORE_PATH).toBeDefined();
       expect(ApiPaths.DATASTORE_PATH).toEqual(DATASTORE_PATH);
     });
+
+    it(`should expose "${EDM_PATH}"`, () => {
+      expect(ApiPaths.EDM_PATH).toBeDefined();
+      expect(ApiPaths.EDM_PATH).toEqual(EDM_PATH);
+    });
+
+    it(`should expose "${LINKING_PATH}"`, () => {
+      expect(ApiPaths.LINKING_PATH).toBeDefined();
+      expect(ApiPaths.LINKING_PATH).toEqual(LINKING_PATH);
+    });
+
+    it(`should expose "${ORGANIZATIONS_PATH}"`, () => {
+      expect(ApiPaths.ORGANIZATIONS_PATH).toBeDefined();
+      expect(ApiPaths.ORGANIZATIONS_PATH).toEqual(ORGANIZATIONS_PATH);
+    });
+
+    it(`should expose "${PERMISSIONS_PATH}"`, () => {
+      expect(ApiPaths.PERMISSIONS_PATH).toBeDefined();
+      expect(ApiPaths.PERMISSIONS_PATH).toEqual(PERMISSIONS_PATH);
+    });
+
+    it(`should expose "${PRINCIPALS_PATH}"`, () => {
+      expect(ApiPaths.PRINCIPALS_PATH).toBeDefined();
+      expect(ApiPaths.PRINCIPALS_PATH).toEqual(PRINCIPALS_PATH);
+    });
+
+    it(`should expose "${REQUESTS_PATH}"`, () => {
+      expect(ApiPaths.REQUESTS_PATH).toBeDefined();
+      expect(ApiPaths.REQUESTS_PATH).toEqual(REQUESTS_PATH);
+    });
+
+    it(`should expose "${SEARCH_PATH}"`, () => {
+      expect(ApiPaths.SEARCH_PATH).toBeDefined();
+      expect(ApiPaths.SEARCH_PATH).toEqual(SEARCH_PATH);
+    });
+
+  });
+
+  describe('shared paths', () => {
 
     it(`should expose "${ENTITY_SET_PATH}"`, () => {
       expect(ApiPaths.ENTITY_SET_PATH).toBeDefined();
@@ -76,11 +125,6 @@ describe('ApiPaths', () => {
     it(`should expose "${NAMESPACE_PATH}"`, () => {
       expect(ApiPaths.NAMESPACE_PATH).toBeDefined();
       expect(ApiPaths.NAMESPACE_PATH).toEqual(NAMESPACE_PATH);
-    });
-
-    it(`should expose "${PRINCIPALS_PATH}"`, () => {
-      expect(ApiPaths.PRINCIPALS_PATH).toBeDefined();
-      expect(ApiPaths.PRINCIPALS_PATH).toEqual(PRINCIPALS_PATH);
     });
 
     it(`should expose "${PROPERTY_TYPE_PATH}"`, () => {
@@ -105,21 +149,7 @@ describe('ApiPaths', () => {
 
   });
 
-  describe('AuthorizationApi specific paths', () => {
-
-    it(`should expose "${AUTHORIZATIONS_PATH}"`, () => {
-      expect(ApiPaths.AUTHORIZATIONS_PATH).toBeDefined();
-      expect(ApiPaths.AUTHORIZATIONS_PATH).toEqual(AUTHORIZATIONS_PATH);
-    });
-
-  });
-
   describe('DataApi specific paths', () => {
-
-    it(`should expose "${DATA_PATH}"`, () => {
-      expect(ApiPaths.DATA_PATH).toBeDefined();
-      expect(ApiPaths.DATA_PATH).toEqual(DATA_PATH);
-    });
 
     it(`should expose "${ENTITY_DATA_PATH}"`, () => {
       expect(ApiPaths.ENTITY_DATA_PATH).toBeDefined();
@@ -133,34 +163,11 @@ describe('ApiPaths', () => {
 
   });
 
-  describe('DataSourcesApi specific paths', () => {
-
-    it(`should expose "${DATA_SOURCES_PATH}"`, () => {
-      expect(ApiPaths.DATA_SOURCES_PATH).toBeDefined();
-      expect(ApiPaths.DATA_SOURCES_PATH).toEqual(DATA_SOURCES_PATH);
-    });
-
-  });
-
   describe('EntityDataModelApi specific paths', () => {
-
-    it(`should expose "${EDM_PATH}"`, () => {
-      expect(ApiPaths.EDM_PATH).toBeDefined();
-      expect(ApiPaths.EDM_PATH).toEqual(EDM_PATH);
-    });
 
     it(`should expose "${SCHEMA_PATH}"`, () => {
       expect(ApiPaths.SCHEMA_PATH).toBeDefined();
       expect(ApiPaths.SCHEMA_PATH).toEqual(SCHEMA_PATH);
-    });
-
-  });
-
-  describe('LinkingApi specific paths', () => {
-
-    it(`should expose "${LINKING_PATH}"`, () => {
-      expect(ApiPaths.LINKING_PATH).toBeDefined();
-      expect(ApiPaths.LINKING_PATH).toEqual(LINKING_PATH);
     });
 
   });
@@ -182,23 +189,9 @@ describe('ApiPaths', () => {
       expect(ApiPaths.MEMBERS_PATH).toEqual(MEMBERS_PATH);
     });
 
-    it(`should expose "${ORGANIZATIONS_PATH}"`, () => {
-      expect(ApiPaths.ORGANIZATIONS_PATH).toBeDefined();
-      expect(ApiPaths.ORGANIZATIONS_PATH).toEqual(ORGANIZATIONS_PATH);
-    });
-
     it(`should expose "${TITLE_PATH}"`, () => {
       expect(ApiPaths.TITLE_PATH).toBeDefined();
       expect(ApiPaths.TITLE_PATH).toEqual(TITLE_PATH);
-    });
-
-  });
-
-  describe('PermissionsApi specific paths', () => {
-
-    it(`should expose "${PERMISSIONS_PATH}"`, () => {
-      expect(ApiPaths.PERMISSIONS_PATH).toBeDefined();
-      expect(ApiPaths.PERMISSIONS_PATH).toEqual(PERMISSIONS_PATH);
     });
 
   });
@@ -212,20 +205,21 @@ describe('ApiPaths', () => {
 
   });
 
-  describe('RequestsApi specific paths', () => {
-
-    it(`should expose "${REQUESTS_PATH}"`, () => {
-      expect(ApiPaths.REQUESTS_PATH).toBeDefined();
-      expect(ApiPaths.REQUESTS_PATH).toEqual(REQUESTS_PATH);
-    });
-
-  });
-
   describe('SearchApi specific paths', () => {
 
-    it(`should expose "${SEARCH_PATH}"`, () => {
-      expect(ApiPaths.SEARCH_PATH).toBeDefined();
-      expect(ApiPaths.SEARCH_PATH).toEqual(SEARCH_PATH);
+    it(`should expose "${ADVANCED_PATH}"`, () => {
+      expect(ApiPaths.ADVANCED_PATH).toBeDefined();
+      expect(ApiPaths.ADVANCED_PATH).toEqual(ADVANCED_PATH);
+    });
+
+    it(`should expose "${SEARCH_ENTITY_TYPES_PATH}"`, () => {
+      expect(ApiPaths.SEARCH_ENTITY_TYPES_PATH).toBeDefined();
+      expect(ApiPaths.SEARCH_ENTITY_TYPES_PATH).toEqual(SEARCH_ENTITY_TYPES_PATH);
+    });
+
+    it(`should expose "${SEARCH_PROPERTY_TYPES_PATH}"`, () => {
+      expect(ApiPaths.SEARCH_PROPERTY_TYPES_PATH).toBeDefined();
+      expect(ApiPaths.SEARCH_PROPERTY_TYPES_PATH).toEqual(SEARCH_PROPERTY_TYPES_PATH);
     });
 
   });
