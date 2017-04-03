d52c1ffe26c601a20dd37203eef7a9c6f19be068
Hristo Oskov <hristo@kryptnostic.com>
2017-04-03 10:55:34 -0700

    add new SecurableObjectTypes

--- test/constants/SecurableTypes.test.js
+++ test/constants/SecurableTypes.test.js
@@ -1,16 +1,25 @@
 import SecurableTypes from '../../src/constants/types/SecurableTypes';
 
+const ComplexType :string = 'ComplexType';
+const EdgeType :string = 'EdgeType';
 const EntitySet :string = 'EntitySet';
 const EntityType :string = 'EntityType';
-const PropertyTypeInEntitySet :string = 'PropertyTypeInEntitySet';
 const DataSource :string = 'Datasource';
+const LinkingEntityType :string = 'LinkingEntityType';
+const PropertyTypeInEntitySet :string = 'PropertyTypeInEntitySet';
 const Organization :string = 'Organization';
+const OrganizationRole :string = 'OrganizationRole';
 
 describe('SecurableTypes', () => {
 
-  it(`should expose "${EntityType}"`, () => {
-    expect(SecurableTypes.EntityType).toBeDefined();
-    expect(SecurableTypes.EntityType).toEqual(EntityType);
+  it(`should expose "${ComplexType}"`, () => {
+    expect(SecurableTypes.ComplexType).toBeDefined();
+    expect(SecurableTypes.ComplexType).toEqual(ComplexType);
+  });
+
+  it(`should expose "${EdgeType}"`, () => {
+    expect(SecurableTypes.EdgeType).toBeDefined();
+    expect(SecurableTypes.EdgeType).toEqual(EdgeType);
   });
 
   it(`should expose "${EntitySet}"`, () => {
@@ -18,9 +27,9 @@ describe('SecurableTypes', () => {
     expect(SecurableTypes.EntitySet).toEqual(EntitySet);
   });
 
-  it(`should expose "${PropertyTypeInEntitySet}"`, () => {
-    expect(SecurableTypes.PropertyTypeInEntitySet).toBeDefined();
-    expect(SecurableTypes.PropertyTypeInEntitySet).toEqual(PropertyTypeInEntitySet);
+  it(`should expose "${EntityType}"`, () => {
+    expect(SecurableTypes.EntityType).toBeDefined();
+    expect(SecurableTypes.EntityType).toEqual(EntityType);
   });
 
   it(`should expose "${DataSource}"`, () => {
@@ -28,9 +37,24 @@ describe('SecurableTypes', () => {
     expect(SecurableTypes.DataSource).toEqual(DataSource);
   });
 
+  it(`should expose "${LinkingEntityType}"`, () => {
+    expect(SecurableTypes.LinkingEntityType).toBeDefined();
+    expect(SecurableTypes.LinkingEntityType).toEqual(LinkingEntityType);
+  });
+
+  it(`should expose "${PropertyTypeInEntitySet}"`, () => {
+    expect(SecurableTypes.PropertyTypeInEntitySet).toBeDefined();
+    expect(SecurableTypes.PropertyTypeInEntitySet).toEqual(PropertyTypeInEntitySet);
+  });
+
   it(`should expose "${Organization}"`, () => {
     expect(SecurableTypes.Organization).toBeDefined();
     expect(SecurableTypes.Organization).toEqual(Organization);
+  });
+
+  it(`should expose "${OrganizationRole}"`, () => {
+    expect(SecurableTypes.OrganizationRole).toBeDefined();
+    expect(SecurableTypes.OrganizationRole).toEqual(OrganizationRole);
   });
 
 });
