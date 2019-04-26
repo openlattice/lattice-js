a15c3346ab07e91383024e444b7433a1431d400f
Giulia Campana <giuliaecampana@gmail.com>
2019-04-25 18:25:37 -0700

    Factor out constants used in SearchApi that are also needed in DataApi

--- src/constants/GlobalConstants.js
+++ src/constants/GlobalConstants.js
@@ -6,6 +6,22 @@ import FullyQualifiedName from '../models/FullyQualifiedName';
 
 export const AT_CLASS :string = '@class';
 export const AT_ID :string = '@id';
-
+export const DESTINATION :string = 'dst';
+export const DESTINATION_ES_IDS = 'destinationEntitySetIds';
+export const EDGE :string = 'edge';
+export const EDGE_ES_IDS = 'edgeEntitySetIds';
+export const ENTITY_KEY_IDS :string = 'entityKeyIds';
+export const ENTITY_TYPE_ID :string = 'entityTypeId';
+export const FUZZY :string = 'fuzzy';
+export const KEYWORD :string = 'kw';
+export const MAX_HITS :string = 'maxHits';
+export const NAME :string = 'name';
+export const NAMESPACE :string = 'namespace';
 export const OPENLATTICE_ID_FQN :string = FullyQualifiedName.toString('openlattice', AT_ID);
+export const PROPERTY_TYPE_IDS :string = 'pid';
 export const ROLE_PKG :string = 'com.openlattice.organization.roles.Role';
+export const SEARCH_FIELDS :string = 'searchFields';
+export const SEARCH_TERM :string = 'searchTerm';
+export const SOURCE :string = 'src';
+export const SOURCE_ES_IDS = 'sourceEntitySetIds';
+export const START :string = 'start';
--- src/constants/GlobalConstants.test.js
+++ src/constants/GlobalConstants.test.js
@@ -6,8 +6,25 @@ import * as GlobalConstants from './GlobalConstants';
 const EXPECTED = Map({
   AT_CLASS           : '@class',
   AT_ID              : '@id',
+  DESTINATION        : 'dst',
+  DESTINATION_ES_IDS : 'destinationEntitySetIds',
+  EDGE               : 'edge',
+  EDGE_ES_IDS        : 'edgeEntitySetIds',
+  ENTITY_KEY_IDS     : 'entityKeyIds',
+  ENTITY_TYPE_ID     : 'entityTypeId',
+  FUZZY              : 'fuzzy',
+  KEYWORD            : 'kw',
+  MAX_HITS           : 'maxHits',
+  NAME               : 'name',
+  NAMESPACE          : 'namespace',
   OPENLATTICE_ID_FQN : 'openlattice.@id',
+  PROPERTY_TYPE_IDS  : 'pid',
   ROLE_PKG           : 'com.openlattice.organization.roles.Role',
+  SEARCH_FIELDS      : 'searchFields',
+  SEARCH_TERM        : 'searchTerm',
+  SOURCE             : 'src',
+  SOURCE_ES_IDS      : 'sourceEntitySetIds',
+  START              : 'start',
 }).sortBy((value, key) => key);
 /* eslint-enable */
 
