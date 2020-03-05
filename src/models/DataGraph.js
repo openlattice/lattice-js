2e59fc4d77feef2d5a96ddb5c56f849bce0560c2
Hristo Oskov <hristo@openlattice.com>
2020-03-04 16:15:19 -0800

    DataGraph

--- src/models/DataGraph.js
+++ src/models/DataGraph.js
@@ -1,117 +1,210 @@
 /*
  * @flow
  */
-import { Map, fromJS } from 'immutable';
+
+import {
+  Map,
+  fromJS,
+  isCollection,
+  isImmutable,
+} from 'immutable';
 
 import Logger from '../utils/Logger';
 import { isDefined } from '../utils/LangUtils';
 import { isValidMultimap, isValidOrEmptyMultimap, isValidUUID } from '../utils/ValidationUtils';
-
+import { genRandomString, genRandomUUID } from '../utils/testing/MockUtils';
 
 const LOG = new Logger('DataGraph');
 
-/**
- * @class DataGraph
- * @memberof lattice
- */
-export default class DataGraph {
+type DataGraphObject = {|
+  associations :Object;
+  entities :Object;
+|};
+
+class DataGraph {
 
   associations :Object;
   entities :Object;
 
-  constructor(associations :Object, entities :Object) {
+  constructor(dataGraph :{
+    associations :Object;
+    entities :Object;
+  }) {
 
-    // required properties
-    this.associations = associations;
-    this.entities = entities;
+    this.associations = dataGraph.associations;
+    this.entities = dataGraph.entities;
   }
 
-  asImmutable() :Map<string, Object> {
+  toImmutable() :Map<*, *> {
 
-    const plainObj = {};
-
-    // required properties
-    plainObj.associations = this.associations;
-    plainObj.entities = this.entities;
-
-    return fromJS(plainObj);
+    return fromJS(this.toObject());
   }
 
-  valueOf() {
-    return JSON.stringify({
+  toObject() :DataGraphObject {
+
+    const aceObj :DataGraphObject = {
       associations: this.associations,
       entities: this.entities,
-    });
+    };
+
+    return aceObj;
+  }
+
+  valueOf() :number {
+
+    return this.toImmutable().hashCode();
   }
 }
 
-/**
- * @class DataGraphBuilder
- * @memberof lattice
- */
-export class DataGraphBuilder {
+class DataGraphBuilder {
 
-  associations :Object; // ListMultimap<UUID, DataAssociation>
-  entities :Object; // ListMultimap<UUID, SetMultimap<UUID, Object>>
+  associations :Object;
+  entities :Object;
+
+  constructor(value :any) {
+
+    if (isImmutable(value)) {
+      this.setAssociations(value.get('associations'));
+      this.setEntities(value.get('entities'));
+    }
+    else if (isDefined(value)) {
+      this.setAssociations(value.associations);
+      this.setEntities(value.entities);
+    }
+  }
 
   setAssociations(associations :Object) :DataGraphBuilder {
 
-    if (!isValidOrEmptyMultimap(associations, isValidUUID)) {
-      throw new Error('invalid parameter: associations must be a non-empty object where all values are multimaps');
+    // TODO: still need to validate that associations are valid DataAssociation objects
+
+    let theAssociations = associations;
+    if (isCollection(associations)) {
+      theAssociations = associations.toJS();
     }
 
-    // TODO: still need to validate that values are valid DataAssociation objects
+    if (!isValidOrEmptyMultimap(theAssociations, isValidUUID)) {
+      throw new Error('invalid parameter: "associations" must be a non-empty object where all values are multimaps');
+    }
 
-    this.associations = associations;
+    this.associations = theAssociations;
     return this;
   }
 
   setEntities(entities :Object) :DataGraphBuilder {
 
-    if (!isValidMultimap(entities, isValidUUID)) {
-      throw new Error('invalid parameter: entities must be a non-empty object where all values are multimaps');
+    // TODO: still need to validate that entities are valid multimap objects
+
+    let theEntities = entities;
+    if (isCollection(entities)) {
+      theEntities = entities.toJS();
     }
 
-    // TODO: still need to validate that values are valid multimap objects
+    if (!isValidMultimap(theEntities, isValidUUID)) {
+      throw new Error('invalid parameter: "entities" must be a non-empty object where all values are multimaps');
+    }
 
-    this.entities = entities;
+    this.entities = theEntities;
     return this;
   }
 
   build() :DataGraph {
 
     if (!this.associations) {
-      throw new Error('missing property: associations is a required property');
+      throw new Error('missing property: "associations" is a required property');
     }
 
     if (!this.entities) {
-      throw new Error('missing property: entities is a required property');
+      throw new Error('missing property: "entities" is a required property');
     }
 
-    return new DataGraph(this.associations, this.entities);
+    return new DataGraph({
+      associations: this.associations,
+      entities: this.entities,
+    });
   }
 }
 
-export function isValidDataGraph(data :any) :boolean {
+function isValidDataGraph(value :any) :boolean {
 
-  if (!isDefined(data)) {
-
-    LOG.error('invalid parameter: data must be defined', data);
+  if (!isDefined(value)) {
+    LOG.error('invalid parameter: "value" is not defined');
     return false;
   }
 
   try {
-
-    (new DataGraphBuilder())
-      .setAssociations(data.associations)
-      .setEntities(data.entities)
-      .build();
-
+    (new DataGraphBuilder(value)).build();
     return true;
   }
   catch (e) {
-
-    LOG.error(e, data);
+    LOG.error(e.message, value);
     return false;
   }
 }
+
+export {
+  DataGraph,
+  DataGraphBuilder,
+  isValidDataGraph,
+};
+
+export type {
+  DataGraphObject,
+};
+
+/*
+ *
+ * testing
+ *
+ */
+
+const MOCK_DATA_GRAPH = (new DataGraphBuilder())
+  .setAssociations({
+    'f914f31a-6486-4717-929d-dccecab05c47': [{
+      data: {
+        '87f38161-9c95-4166-9721-8514882dac22': ['2020-02-02']
+      },
+      dstEntityKeyId: 'ff0e0000-0000-0000-8000-00000000fc5e',
+      dstEntitySetId: 'ccdaba20-f6ba-401c-a63d-17c6578ffb67',
+      srcEntityIndex: 0,
+      srcEntitySetId: 'd6760122-eaf7-42e6-9339-923df3f4790a',
+    }]
+  })
+  .setEntities({
+    'd6760122-eaf7-42e6-9339-923df3f4790a': [{
+      'a791ca8d-b433-4a2b-be04-43d43cea14a7': ['VALUE_1'],
+      '2a45205e-703c-43eb-a060-921bf7245f6a': ['VALUE_2', 'VALUE_3'],
+    }],
+  })
+  .build();
+
+const MOCK_DATA_GRAPH_OBJECT = MOCK_DATA_GRAPH.toObject();
+
+function genRandomDataGraph() {
+  return (new DataGraphBuilder())
+    .setAssociations({
+      [genRandomUUID()]: [{
+        dstEntityIndex: 2,
+        dstEntityKeyId: genRandomUUID(),
+        dstEntitySetId: genRandomUUID(),
+        srcEntityIndex: 4,
+        srcEntityKeyId: genRandomUUID(),
+        srcEntitySetId: genRandomUUID(),
+      }],
+    })
+    .setEntities({
+      [genRandomUUID()]: [{
+        [genRandomUUID()]: [genRandomString()],
+        [genRandomUUID()]: [genRandomString(), genRandomString()],
+      }],
+      [genRandomUUID()]: [{
+        [genRandomUUID()]: [genRandomString()],
+      }],
+    })
+    .build();
+}
+
+export {
+  MOCK_DATA_GRAPH,
+  MOCK_DATA_GRAPH_OBJECT,
+  genRandomDataGraph,
+};
--- src/models/DataGraph.test.js
+++ src/models/DataGraph.test.js
@@ -1,25 +1,68 @@
-import { Map, Set } from 'immutable';
-
-import DataGraph, { DataGraphBuilder, isValidDataGraph as isValid } from './DataGraph';
-import { INVALID_PARAMS, INVALID_PARAMS_FOR_OPTIONAL_OBJECT } from '../utils/testing/Invalid';
+import { Map, Set, fromJS } from 'immutable';
 
 import {
-  MOCK_DATA_GRAPH_DM as MOCK_DM,
+  MOCK_DATA_GRAPH,
+  MOCK_DATA_GRAPH_OBJECT,
+  DataGraph,
+  DataGraphBuilder,
   genRandomDataGraph,
-} from '../utils/testing/MockData';
+  isValidDataGraph as isValid,
+} from './DataGraph';
 
-const {
-  associations: mockAssociations,
-  entities: mockEntities,
-} = MOCK_DM;
+import { INVALID_PARAMS, INVALID_PARAMS_FOR_OPTIONAL_OBJECT } from '../utils/testing/Invalid';
+
+function expectValidInstance(value) {
+
+  expect(value).toBeInstanceOf(DataGraph);
+
+  expect(value.associations).toBeDefined();
+  expect(value.entities).toBeDefined();
+
+  expect(value.associations).toEqual(MOCK_DATA_GRAPH.associations);
+  expect(value.entities).toEqual(MOCK_DATA_GRAPH.entities);
+}
 
 describe('DataGraph', () => {
 
   describe('DataGraphBuilder', () => {
 
+    describe('constructor()', () => {
+
+      test('should construct given an instance', () => {
+        expectValidInstance(
+          (new DataGraphBuilder(MOCK_DATA_GRAPH)).build()
+        );
+      });
+
+      test('should construct given an object literal', () => {
+        expectValidInstance(
+          (new DataGraphBuilder({ ...MOCK_DATA_GRAPH })).build()
+        );
+        expectValidInstance(
+          (new DataGraphBuilder(MOCK_DATA_GRAPH_OBJECT)).build()
+        );
+      });
+
+      test('should construct given an immutable object', () => {
+        expectValidInstance(
+          (new DataGraphBuilder(MOCK_DATA_GRAPH.toImmutable())).build()
+        );
+        expectValidInstance(
+          (new DataGraphBuilder(fromJS({ ...MOCK_DATA_GRAPH }))).build()
+        );
+        expectValidInstance(
+          (new DataGraphBuilder(fromJS(MOCK_DATA_GRAPH_OBJECT))).build()
+        );
+      });
+
+    });
+
     describe('setAssociations()', () => {
 
       test('should throw when given invalid parameters', () => {
+        expect(() => {
+          (new DataGraphBuilder()).setAssociations();
+        }).toThrow();
         INVALID_PARAMS_FOR_OPTIONAL_OBJECT.forEach((invalidInput) => {
           expect(() => {
             (new DataGraphBuilder()).setAssociations(invalidInput);
@@ -27,15 +70,9 @@ describe('DataGraph', () => {
         });
       });
 
-      test('should throw when not given any parameters', () => {
-        expect(() => {
-          (new DataGraphBuilder()).setAssociations();
-        }).toThrow();
-      });
-
       test('should not throw when given valid parameters', () => {
         expect(() => {
-          (new DataGraphBuilder()).setAssociations(mockAssociations);
+          (new DataGraphBuilder()).setAssociations(MOCK_DATA_GRAPH.associations);
         }).not.toThrow();
       });
 
@@ -44,6 +81,9 @@ describe('DataGraph', () => {
     describe('setEntities()', () => {
 
       test('should throw when given invalid parameters', () => {
+        expect(() => {
+          (new DataGraphBuilder()).setEntities();
+        }).toThrow();
         INVALID_PARAMS.forEach((invalidInput) => {
           expect(() => {
             (new DataGraphBuilder()).setEntities(invalidInput);
@@ -51,15 +91,9 @@ describe('DataGraph', () => {
         });
       });
 
-      test('should throw when not given any parameters', () => {
-        expect(() => {
-          (new DataGraphBuilder()).setEntities();
-        }).toThrow();
-      });
-
       test('should not throw when given valid parameters', () => {
         expect(() => {
-          (new DataGraphBuilder()).setEntities(mockEntities);
+          (new DataGraphBuilder()).setEntities(MOCK_DATA_GRAPH.entities);
         }).not.toThrow();
       });
 
@@ -70,14 +104,16 @@ describe('DataGraph', () => {
       test('should throw when a required property has not been set', () => {
 
         expect(() => {
+          // omitting setAssociations
           (new DataGraphBuilder())
-            .setEntities(mockEntities)
+            .setEntities(MOCK_DATA_GRAPH.entities)
             .build();
         }).toThrow();
 
         expect(() => {
+          // omitting setEntities
           (new DataGraphBuilder())
-            .setAssociations(mockAssociations)
+            .setAssociations(MOCK_DATA_GRAPH.associations)
             .build();
         }).toThrow();
       });
@@ -85,13 +121,11 @@ describe('DataGraph', () => {
       test('should return a valid instance', () => {
 
         const dataGraph = (new DataGraphBuilder())
-          .setAssociations(mockAssociations)
-          .setEntities(mockEntities)
+          .setAssociations(MOCK_DATA_GRAPH.associations)
+          .setEntities(MOCK_DATA_GRAPH.entities)
           .build();
 
-        expect(dataGraph).toBeInstanceOf(DataGraph);
-        expect(dataGraph.associations).toEqual(mockAssociations);
-        expect(dataGraph.entities).toEqual(mockEntities);
+        expectValidInstance(dataGraph);
       });
 
     });
@@ -103,25 +137,11 @@ describe('DataGraph', () => {
     describe('valid', () => {
 
       test('should return true when given a valid object literal', () => {
-        expect(isValid(MOCK_DM)).toEqual(true);
+        expect(isValid(MOCK_DATA_GRAPH_OBJECT)).toEqual(true);
       });
 
-      test('should return true when given a valid object instance ', () => {
-        expect(isValid(
-          new DataGraph(
-            mockAssociations, mockEntities
-          )
-        )).toEqual(true);
-      });
-
-      test('should return true when given an instance constructed by the builder', () => {
-
-        const dataGraph = (new DataGraphBuilder())
-          .setAssociations(mockAssociations)
-          .setEntities(mockEntities)
-          .build();
-
-        expect(isValid(dataGraph)).toEqual(true);
+      test('should return true when given a valid instance ', () => {
+        expect(isValid(MOCK_DATA_GRAPH)).toEqual(true);
       });
 
     });
@@ -140,22 +160,23 @@ describe('DataGraph', () => {
 
       test('should return false when given an object literal with an invalid "associations" property', () => {
         INVALID_PARAMS_FOR_OPTIONAL_OBJECT.forEach((invalidInput) => {
-          expect(isValid({ ...MOCK_DM, associations: invalidInput })).toEqual(false);
+          expect(isValid({ ...MOCK_DATA_GRAPH_OBJECT, associations: invalidInput })).toEqual(false);
         });
       });
 
       test('should return false when given an object literal with an invalid "entities" property', () => {
         INVALID_PARAMS.forEach((invalidInput) => {
-          expect(isValid({ ...MOCK_DM, entities: invalidInput })).toEqual(false);
+          expect(isValid({ ...MOCK_DATA_GRAPH_OBJECT, entities: invalidInput })).toEqual(false);
         });
       });
 
       test('should return false when given an instance with an invalid "associations" property', () => {
         INVALID_PARAMS_FOR_OPTIONAL_OBJECT.forEach((invalidInput) => {
           expect(isValid(
-            new DataGraph(
-              invalidInput, mockEntities
-            )
+            new DataGraph({
+              associations: invalidInput,
+              entities: MOCK_DATA_GRAPH.entities,
+            })
           )).toEqual(false);
         });
       });
@@ -163,9 +184,10 @@ describe('DataGraph', () => {
       test('should return false when given an instance with an invalid "entities" property', () => {
         INVALID_PARAMS.forEach((invalidInput) => {
           expect(isValid(
-            new DataGraph(
-              mockAssociations, invalidInput
-            )
+            new DataGraph({
+              associations: MOCK_DATA_GRAPH.associations,
+              entities: invalidInput,
+            })
           )).toEqual(false);
         });
       });
@@ -177,54 +199,51 @@ describe('DataGraph', () => {
   describe('equality', () => {
 
     test('valueOf()', () => {
-      const dataGraph = new DataGraph(mockAssociations, mockEntities);
-      expect(dataGraph.valueOf()).toEqual(JSON.stringify({
-        associations: mockAssociations,
-        entities: mockEntities,
-      }));
+      expect(MOCK_DATA_GRAPH.valueOf()).toEqual(
+        fromJS({
+          associations: MOCK_DATA_GRAPH.associations,
+          entities: MOCK_DATA_GRAPH.entities,
+        }).hashCode()
+      );
     });
 
     test('Immutable.Set', () => {
 
-      const {
-        associations: randomAssociations,
-        entities: randomEntities,
-      } = genRandomDataGraph();
-      const testSet = Set().withMutations((mutableSet) => {
-        mutableSet.add(new DataGraph(mockAssociations, mockEntities));
-        mutableSet.add(new DataGraph(randomAssociations, randomEntities));
-        mutableSet.add(new DataGraph(mockAssociations, mockEntities));
-      });
+      const randomDataGraph = genRandomDataGraph();
+      const dataGraph0 = (new DataGraphBuilder(MOCK_DATA_GRAPH)).build();
+      const dataGraph1 = (new DataGraphBuilder(MOCK_DATA_GRAPH)).build();
+
+      const testSet = Set()
+        .add(dataGraph0)
+        .add(randomDataGraph)
+        .add(dataGraph1);
+
       expect(testSet.size).toEqual(2);
       expect(testSet.count()).toEqual(2);
 
-      expect(testSet.first().associations).toEqual(mockAssociations);
-      expect(testSet.first().entities).toEqual(mockEntities);
+      expect(testSet.first().associations).toEqual(MOCK_DATA_GRAPH.associations);
+      expect(testSet.first().entities).toEqual(MOCK_DATA_GRAPH.entities);
 
-      expect(testSet.last().associations).toEqual(randomAssociations);
-      expect(testSet.last().entities).toEqual(randomEntities);
+      expect(testSet.last().associations).toEqual(randomDataGraph.associations);
+      expect(testSet.last().entities).toEqual(randomDataGraph.entities);
     });
 
     test('Immutable.Map', () => {
 
-      const {
-        associations: randomAssociations,
-        entities: randomEntities,
-      } = genRandomDataGraph();
-      const dataGraph0 = new DataGraph(mockAssociations, mockEntities);
-      const dataGraph1 = new DataGraph(randomAssociations, randomEntities);
-      const dataGraph2 = new DataGraph(mockAssociations, mockEntities);
+      const randomDataGraph = genRandomDataGraph();
+      const dataGraph0 = (new DataGraphBuilder(MOCK_DATA_GRAPH)).build();
+      const dataGraph1 = (new DataGraphBuilder(MOCK_DATA_GRAPH)).build();
 
-      const testMap = Map().withMutations((mutableMap) => {
-        mutableMap.set(dataGraph0, 'test_value_1');
-        mutableMap.set(dataGraph1, 'test_value_2');
-        mutableMap.set(dataGraph2, 'test_value_3');
-      });
+      const testMap = Map()
+        .set(dataGraph0, 'test_value_1')
+        .set(randomDataGraph, 'test_value_2')
+        .set(dataGraph1, 'test_value_3');
+
       expect(testMap.size).toEqual(2);
       expect(testMap.count()).toEqual(2);
       expect(testMap.get(dataGraph0)).toEqual('test_value_3');
-      expect(testMap.get(dataGraph1)).toEqual('test_value_2');
-      expect(testMap.get(dataGraph2)).toEqual('test_value_3');
+      expect(testMap.get(randomDataGraph)).toEqual('test_value_2');
+      expect(testMap.get(dataGraph1)).toEqual('test_value_3');
     });
 
   });
