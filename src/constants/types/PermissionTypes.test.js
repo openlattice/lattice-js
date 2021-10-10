11b2c54b3164fd5d41487abc8e5ffaf09c8034fd
Hristo Oskov <hristo@openlattice.com>
2021-10-09 23:17:20 -0700

    new permission type INTEGRATE

--- src/constants/types/PermissionTypes.test.js
+++ src/constants/types/PermissionTypes.test.js
@@ -5,6 +5,7 @@ import { testEnumIntegrity } from '../../utils/testing/TestUtils';
 
 const EXPECTED_ENUM = Map({
   DISCOVER: 'DISCOVER',
+  INTEGRATE: 'INTEGRATE',
   LINK: 'LINK',
   MATERIALIZE: 'MATERIALIZE',
   OWNER: 'OWNER',
